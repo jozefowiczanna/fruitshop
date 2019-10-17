ac8194a8c49b377f07b811b021e00e8f11e0c2c6
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2019-10-17 20:25:57 +0200

    added list of products with sort options

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,261 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="container">
+      <nav class="navbar d-flex fixed-top">
+        <button class="btn btn-outline-success bg-white ml-auto">$</button>
+      </nav>
+    </header>
+    <section class="container">
+      <h1 class="mt-5 mb-4">Fruit Shop</h1>
+      <div class="form-inline my-2">
+        <b>Max cena (zł/kg): </b>
+        <input class="max-input form-control text-center mx-2" maxlength="3"
+          type="text"
+          v-model="maxValue">
+        <input class="max-range custom-range my-3"
+          type="range"
+          min="0" max="10"
+          v-model="maxValue">
+      </div>
+
+      <div class="d-flex mb-5">
+        <b class="mr-3">sortuj wg: </b>
+        <div class="custom-control custom-radio d-flex">
+          <div class="pr-3">
+            <input type="radio" id="price" class="custom-control-input"
+              value="price" v-model="picked">
+            <label for="price" class="custom-control-label">ceny</label>
+          </div>
+          <div class="pl-4">
+            <input type="radio" id="name" class="custom-control-input"
+              value="name" v-model="picked">
+            <label for="name" class="custom-control-label">nazwy</label>
+          </div>
+        </div>
+      </div>
+        
+      <div class="row d-flex flex-wrap">
+        <div class="col-md-6 col-lg-4 mb-5"
+          v-for="(item) in sortProducts"
+          :key="item.id"
+        >
+          <div class="product position-relative">
+            <div class="product__img-wrapper">
+              <div class="product__img" :style="getImg(item.image)"></div>
+            </div>
+            <div class="product__description p-4 d-flex flex-column justify-content-between position-relative">
+              <div class="product__name mt mb-3">{{item.name}}</div>
+              <div class="product__price ml-auto">{{item.price}}<span class="product__currency"> zł</span></div>
+              <button class="add-btn"></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+var products = [ 
+        { 
+          "id":"1",
+          "name":"Pomarańcze",
+          "price":"4,90",
+          "image":"oranges.jpg"
+        },
+        { 
+          "id":"2",
+          "name":"Jabłka",
+          "price":"2,50",
+          "image":"apples.jpg"
+        },
+        { 
+          "id":"3",
+          "name":"Gruszki",
+          "price":"2,90",
+          "image":"pears.jpg"
+        },
+        { 
+          "id":"4",
+          "name":"Banany",
+          "price":"5,80",
+          "image":"bananas.jpg"
+        },
+        { 
+          "id":"5",
+          "name":"Truskawki",
+          "price":"6,70",
+          "image":"strawberries.jpg"
+        },
+        { 
+          "id":"6",
+          "name":"Winogrona",
+          "price":"6,90",
+          "image":"grapes.jpg"
+        },
+        { 
+          "id":"7",
+          "name":"Śliwki",
+          "price":"5.50",
+          "image":"plums.jpg"
+        },
+        { 
+          "id":"8",
+          "name":"Jagody",
+          "price":"7.90",
+          "image":"blueberries.jpg"
+        },
+        { 
+          "id":"9",
+          "name":"Brzoskwinie",
+          "price":"7.50",
+          "image":"peaches.jpg"
+        },
+        { 
+          "id":"10",
+          "name":"Maliny",
+          "price":"8.50",
+          "image":"raspberries.jpg"
+        }   
+      ]
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      maxValue: 10,
+      picked: "price",
+      products: products
+    }
+  },
+  computed: {
+    sortProducts() {
+      var arrayCopy = this.products.slice();
+
+      if (this.picked === "name") {
+
+        arrayCopy.sort((a, b) => (a.name < b.name) ? -1 : 1);
+      } else {
+        arrayCopy.sort(function(a, b) {
+          var priceA = parseFloat(a.price.replace(',', '.'));
+          var priceB = parseFloat(b.price.replace(',', '.'));
+
+          (priceA < priceB) ? -1 : 0;
+        });
+      }
+      var max = this.maxValue;
+      return arrayCopy.filter(function(item) {
+        return parseInt(item.price) < max;
+      })
+    }
+  },
+  methods: {
+    getImg(img) {
+      var url = require(`@/assets/img/${img}`);
+      return {'background-image': `url('${url}')`};
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:600&display=swap');
+
+  .product {
+    box-shadow: 0 10px 50px -25px #89a08d;
+    font-family: 'Montserrat', sans-serif;
+    border-radius: 5px;
+  }
+
+  .product__img-wrapper {
+    position: relative;
+    height: 270px;
+    transition: 0.5s;
+    overflow: hidden;
+    border-radius: 5px 5px 0 0;
+  }
+
+  .product:hover .product__img-wrapper {
+    background-size: 103%;
+  }
+
+  .product__img {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  .product__img {
+    transform: scale(1);
+    transition: all 0.3s;
+  }
+
+  .product:hover .product__img {
+    transform: scale(1.05);
+  }
+
+  .product__name {
+    font-size: 21px;
+  }
+
+  .product__price {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 25px;
+    border: 3px solid #28a745;
+    border-radius: 5px;
+    padding: 5px 10px;
+    background: white;
+    z-index: 50;
+  }
+
+  .product__basket {
+    border-radius: 10px;
+  }
+
+  .add-btn {
+    position: absolute;
+    top: -28px;
+    right: 20px;
+    width: 56px;
+    height: 56px;
+    font-size: 25px;
+    background-color: #28a745;
+    z-index: 50;
+    border: 4px solid white;
+    border-radius: 50%;
+    transition: background-color 0.2s ease-in-out;
+    outline: none !important;
+  }
+
+  .add-btn:hover {
+    background-color: #218838;
+  }
+
+  .add-btn::before {
+    position: absolute;
+    content: '+';
+    font-size: 30px;
+    color: white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .product__currency {
+    font-size: 0.7em;
+  }
+
+  .max-input {
+    width: 60px !important;
+  }
+
+  .max-range {
+    max-width: 200px !important;
+  }
+
+
 </style>
+
